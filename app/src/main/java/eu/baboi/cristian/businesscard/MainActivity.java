645a156a097a2751dc0e0be58aa05e5a106abf65
Cristian Baboi <cristian.baboi@gmail.com>
2018-02-19 11:54:42 +0200

    Performed some refactoring of the Java and XML files

--- app/src/main/java/eu/baboi/cristian/businesscard/MainActivity.java
+++ app/src/main/java/eu/baboi/cristian/businesscard/MainActivity.java
@@ -4,13 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.LinkMovementMethod;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Display;
-import android.view.Surface;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,35 +15,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set the logo dimensions if in landscape
+        // Adjust the logo size for landscape orientation
+        fixLandscape();
 
-        // Get the display
-        Display display = getWindowManager().getDefaultDisplay();
+        // Recognize the links in the address field
+        exposeLinks();
+    }
 
-        // It is landscape?
-        if(display.getRotation()!= Surface.ROTATION_0 ) {
 
-            // Get the display metrics
-            DisplayMetrics metrics = new DisplayMetrics();
-            display.getMetrics(metrics);
+    // This method expose the links in the address field
+    private void exposeLinks() {
+        // Find the address field
+        final TextView addr = (TextView) findViewById(R.id.id_address);
+
+        // See https://discussions.udacity.com/t/how-to-put-links-in-a-textview/507450/2?u=cristian.baboi
+        // Mark the links in the address field
+        addr.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    // This method sets the limits for the logo size to the screen size
+    private void fixLandscape() {
+
+        // Get the display metrics
+        final DisplayMetrics metrics = new DisplayMetrics();
+        getMetrics(metrics);
+
+        // Put bounds to the logo size if we are in landscape orientation
+        if (isLandscape(metrics)) {
 
             // Find the logo
             final ImageView logo = (ImageView) findViewById(R.id.id_logo);
 
-            // Set the logo dimensions
-            logo.setMaxWidth(metrics.widthPixels-((int)(24*metrics.density)));
-            logo.setMaxHeight(metrics.heightPixels-((int)(24*metrics.density)));
-            Log.e("aaaaa",String.valueOf(metrics.density)+"\n"+String.valueOf(metrics.widthPixels)+"\n"+String.valueOf(metrics.heightPixels));
-
+            // Set the logo bounds
+            logo.setMaxWidth(metrics.widthPixels); // not necessary
+            // Set the maxHeight bound to screen height adjusted with the status bar size: 24dp
+            logo.setMaxHeight(metrics.heightPixels - ((int) (24 * metrics.density)));
         }
+    }
 
-        // Recognize the links in the address field
+    /**
+     * This is an utility method for getting the screen metrics
+     *
+     * @param metrics the DisplayMetrics to get
+     */
+    void getMetrics(DisplayMetrics metrics) {
 
-        // Find the address field
-        final TextView addr = (TextView) findViewById(R.id.id_address);
+        // See https://developer.android.com/reference/android/util/DisplayMetrics.html
 
-        // https://discussions.udacity.com/t/how-to-put-links-in-a-textview/507450/2?u=cristian.baboi
-        addr.setMovementMethod(LinkMovementMethod.getInstance());
+        // Get the display
+        final Display display = getWindowManager().getDefaultDisplay();
+
+        // Get the metrics
+        display.getMetrics(metrics);
+    }
+
+    /**
+     * This is an utility method for detecting the metrics orientation
+     *
+     * @param metrics The screen metrics to check
+     * @return true if metrics are for Landscape orientation, false if not
+     */
+    boolean isLandscape(DisplayMetrics metrics) {
+        return (metrics.widthPixels > metrics.heightPixels);
     }
 
 }
